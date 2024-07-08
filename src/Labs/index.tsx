7fdcc343cd7d982eb70406aa63fcd8e805a27660
96philly <156269500+96philly@users.noreply.github.com>
2024-07-08 09:03:37 -0700

    Update index.tsx

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,24 +1,26 @@
-import { Navigate, Route, Routes } from "react-router";
+import {Routes, Route, Navigate} from "react-router-dom";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import TOC from "./TOC";
+import React from "react";
 
 export default function Labs() {
   return (
     <div>
       <h1>YiJie Cao</h1>
+      <h1>CS5610 Section 61065</h1>
       <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="/Lab1" element={<Lab1 />} />
-        <Route path="/Lab2" element={<Lab2 />} />
-        <Route path="/Lab3" element={<Lab3 />} />
-      </Routes>
-      <a href="https://github.com/96philly/kanbas-react-web-app">
-        GitHub
-      </a>
-    </div>
-  );
+     <TOC/>
+            <Routes>
+                <Route path="/" element={<Navigate to="Lab1"/>}/>
+                <Route path="Lab1" element={<Lab1/>}/>
+                <Route path="Lab2" element={<Lab2/>}/>
+                <Route path="Lab3" element={<Lab3/>}/>
+            </Routes>
+      < a href = "https://github.com/96philly/kanbas-react-web-app">
+                GitHub
+            </a>
+        </div>
+    );
 }
