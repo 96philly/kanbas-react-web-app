e3fad998f89017d746dfd846b3f18d5d0e8444a3
96philly <156269500+96philly@users.noreply.github.com>
2024-07-08 04:03:17 -0700

    Update Status.tsx

--- src/Kanbas/Courses/Home/Status.tsx
+++ src/Kanbas/Courses/Home/Status.tsx
@@ -1,15 +1,16 @@
-export default function CourseStatus() {
+import Modules from "../Modules";
+import CourseStatus from "./Status";
+export default function Home() {
   return (
-    <div id="wd-course-status">
-      <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      <button>Import Existing Content</button>
-      <button>Import from Commons</button>
-      <button>Choose Home Page</button>
-      <button>View Course Stream</button>
-      <button>New Announcement</button>
-      <button>New Analytics</button>
-      <button>View Course Notifications</button>
-    </div>
+    <table id="wd-home">
+      <tr>
+        <td valign="top">
+          <Modules />
+        </td>
+        <td valign="top">
+          <CourseStatus />
+        </td>
+      </tr>
+    </table>
   );
 }
