6d97b06adbdb720176e3a6f72a6d705e4ab40b5b
96philly <156269500+96philly@users.noreply.github.com>
2024-07-08 08:54:53 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Labs from "./Labs";
-import { HashRouter, Routes, Route, Navigate } from "react-router-dom";
+import React from 'react';
+import Labs from './Labs';
+import {HashRouter, Navigate, Route, Routes} from "react-router-dom";
 import Kanbas from "./Kanbas";
 
 function App() {
   return (
-    <HashRouter>
-      <div>
+      <HashRouter>
+    <div>
         <Routes>
-          <Route path="/" element={<Navigate to="Labs" />} />
-          <Route path="/Labs/*" element={<Labs />} />
-          <Route path="/Kanbas/*" element={<Kanbas />} />
+            <Route path="/" element={<Navigate to="Labs"/>} />
+            <Route path="/labs/*" element={<Labs />} />
+            <Route path="/Kanbas/*" element={<Kanbas />} />
         </Routes>
         {/* Kanbas */}
-        < a href = "https://github.com/96philly/kanbas-react-web-app"></a>
+        < a href = "https://github.com/96philly/kanbas-react-web-app">
             GitHub
         </a>
     </div>
